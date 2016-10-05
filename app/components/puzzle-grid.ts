f8cfe3a26d193b1cdf1adef41bc2591290f97b91
Bobby Brennan <bobby.brennan@gmail.com>
2016-10-05 02:37:48 +0000

    show clues next to grid

--- app/components/puzzle-grid.ts
+++ app/components/puzzle-grid.ts
@@ -5,15 +5,24 @@ declare let $: any;
     selector: 'puzzle-grid',
     template: `
         <div class="puzzle" *ngIf="grid">
-          <div class="puzzle-row" *ngFor="let row of grid.cells">
-            <div class="puzzle-square" *ngFor="let cell of row"
-                  (click)="puzzleSquareClick(cell)" [class.filled]="cell.filled">
-              <span class="puzzle-number">{{cell.number}}&nbsp;</span>
-              <input *ngIf="!cell.filled"
-                    class="puzzle-value text-uppercase {{cell.autocompleted ? 'autocompleted' : ''}}"
-                    (keyup)="onGridKeyUp($event, cell)"
-                    [(ngModel)]="cell.value">
+          <div class="puzzle-rows pull-left">
+            <div class="puzzle-row" *ngFor="let row of grid.cells">
+              <div class="puzzle-square" *ngFor="let cell of row"
+                    (click)="puzzleSquareClick(cell)" [class.filled]="cell.filled">
+                <span class="puzzle-number">{{cell.number}}&nbsp;</span>
+                <input *ngIf="!cell.filled"
+                      class="puzzle-value text-uppercase {{cell.autocompleted ? 'autocompleted' : ''}}"
+                      (keyup)="onGridKeyUp($event, cell)"
+                      (focus)="activeClues = grid.getCluesForCell(cell)"
+                      [(ngModel)]="cell.value">
+              </div>
             </div>
+          </div>
+          <div class="active-clues" *ngIf="activeClues">
+            <h4>{{activeClues.across.number}}. Across</h4>
+            <p>{{activeClues.across.prompt || '(No clue set)' }}</p>
+            <h4>{{activeClues.down.number}}. Down</h4>
+            <p>{{activeClues.down.prompt || '(No clue set)' }}</p>
           </div>
         </div>
       `,
